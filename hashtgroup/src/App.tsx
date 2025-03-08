634d95e972d28e05ebeddf9c4413aa2bf9b4c10c
Shakiba Alipour <alipour.sh79@gmail.com>
2025-03-08 17:18:30 +0330

    react router dom and react-scroll packages are uninstalled

--- hashtgroup/src/App.tsx
+++ hashtgroup/src/App.tsx
@@ -4,12 +4,12 @@ import Navbar from "./components/Navbar";
 import Description from "./components/Description";
 
 function App() {
-  return (
-    <div className="App w-full flex flex-col items-center">
-      <Navbar/>
-      <Description/>
-    </div>
-  );
+    return (
+        <div className="App w-full flex flex-col items-center">
+            <Navbar/>
+            <Description/>
+        </div>
+    );
 }
 
 export default App;
--- hashtgroup/src/components/Navbar.tsx
+++ hashtgroup/src/components/Navbar.tsx
@@ -1,33 +1,44 @@
 import React, {useEffect, useState} from "react";
-import Logo from "../assets/Logo"
+import MainLogo from "../assets/MainLogo"
+import Logo from "../assets/Logo";
 
-const Navbar=()=>{
+const Navbar = () => {
     // To hold the active path
-    const [activeSection,setActiveSection]=useState("description")
+    const [activeSection, setActiveSection] = useState("description")
+
+    // To check the screen size
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 780);
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth <= 780);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
 
-    const menuItems=[
-        {path:"description",label:"توضیحات", key:"description"},
-        {path:"game",label:"بازی", key:"game"},
-        {path:"invitation",label:"دعوت از دوستان", key:"invitation"},
-        {path:"leaderboard",label:"لیدربورد", key:"leaderboard"}
+    const menuItems = [
+        {path: "description", label: "توضیحات", key: "description"},
+        {path: "game", label: "بازی", key: "game"},
+        {path: "invitation", label: "دعوت از دوستان", key: "invitation"},
+        {path: "leaderboard", label: "لیدربورد", key: "leaderboard"}
     ]
 
     // useEffect({},[])
 
-    return<header className="w-full sticky flex flex-row justify-around mt-4 mb-4 border-b-blue-950 bg-Background"
-                  style={{background:`background: linear-gradient(270deg, rgba(6, 15, 41, 0.6) 0%, rgba(6, 15, 41, 0) 49.5%, rgba(6, 15, 41, 0.6) 100%), url(blue-purple-brick-wall-textured-background.jpg);
-        opacity: 0.4;`}}>
-        <div className="flex flex-row justify-around w-1/3">
+    return <header className="w-full sticky flex flex-row justify-around mt-4 mb-4 border-b-blue-950 bg-Background"
+                   style={{
+                       background: `background: linear-gradient(270deg, rgba(6, 15, 41, 0.6) 0%, rgba(6, 15, 41, 0) 49.5%, rgba(6, 15, 41, 0.6) 100%), url(blue-purple-brick-wall-textured-background.jpg);
+        opacity: 0.4;`
+                   }}>
+        <div className="flex flex-row justify-around w-3/5">
             {menuItems.map(({path, label, key}) => {
-                const isActive=""
+                const isActive = activeSection === path
                 return <div key={key}>
                     <a href={path}>{label}</a>
-                    {isActive&& <span className="absolute bg-Light w-1 h-1 rounded-full bottom-[-10px]"/>}
+                    {isActive && <span className="absolute bg-Light w-2 h-2 rounded-full top-[10px]"/>}
                 </div>
             })}
         </div>
-        <Logo />
+        {isMobile ? <Logo/> : <MainLogo/>}
     </header>
 }
 
